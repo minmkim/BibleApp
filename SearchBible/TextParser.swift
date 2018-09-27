553c06787322fea87ec79816a77b56351bc65321
Min Kim <minmkim@gmail.com>
2018-09-26 22:57:31 -0400

    fixing text parses for intents

--- SearchBible/TextParser.swift
+++ SearchBible/TextParser.swift
@@ -10,6 +10,13 @@ import Foundation
 
 class TextParser {
     
+    enum TypeOfBook {
+        case numberBook
+        case regularBook
+    }
+    
+    var typeOfBook = TypeOfBook.regularBook
+    
     func parseVerse(for clipboardText: String) -> [String:String] {
         let splitText = clipboardText.components(separatedBy: " ")
         var returnDict = [String: String]()
@@ -19,43 +26,70 @@ class TextParser {
         return returnDict
     }
     
+    var bookNumberNames = ["first", "1st", "1", "second", "2nd", "2", "third", "3rd", "3"]
+    
     func calculatebook(for splitText: [String]) -> String {
+        
+        if bookNumberNames.contains(splitText[0].lowercased()) {
+            guard let index = bookNumberNames.firstIndex(of: splitText[0].lowercased()) else { fatalError()}
+            typeOfBook = .numberBook
+            switch index {
+            case 0...2:
+                return "1 \(splitText[1])"
+            case 3...5:
+                return "2 \(splitText[1])"
+            case 6...8:
+                return "3 \(splitText[1])"
+            default:
+                print("error")
+            }
+        }
+        
         var book = splitText.first!
         book = book.capitalizingFirstLetter()
         return book
     }
     
     func calculateChapter(for splitText: [String]) -> Int {
+        var index = 1
+        if typeOfBook == .numberBook {
+            index += 1
+        }
+        
         var chapter = 1
-        if splitText[1].lowercased() == "chapter" {
-            if Int(splitText[2]) == nil {
-                chapter = returnNumber(for: splitText[2])
+        if splitText[index].lowercased() == "chapter" {
+            if Int(splitText[index + 1]) == nil {
+                chapter = returnNumber(for: splitText[index + 1])
             } else {
-                chapter = Int(splitText[2])!
+                chapter = Int(splitText[index + 1])!
             }
         } else {
-            if Int(splitText[1]) != nil {
-                chapter = Int(splitText[1])!
+            if Int(splitText[index + 1]) != nil {
+                chapter = Int(splitText[index])!
             } else {
-                chapter = returnNumber(for: splitText[1])
+                chapter = returnNumber(for: splitText[index])
             }
         }
         return chapter
     }
     
     func calculateVerse(for splitText: [String]) -> Int {
+        var index = 2
+        if typeOfBook == .numberBook {
+            index += 1
+        }
         var verse = 1
-        if splitText[2].lowercased() == "verse" || splitText[2].lowercased() == "verses" {
-            if Int(splitText[3]) == nil {
+        if splitText[index].lowercased() == "verse" || splitText[index].lowercased() == "verses" {
+            if Int(splitText[index + 1]) == nil {
                 verse = returnNumber(for: splitText[3])
             } else {
-                verse = Int(splitText[3])!
+                verse = Int(splitText[index + 1])!
             }
         } else {
-            if Int(splitText[4]) != nil {
-                verse = Int(splitText[4])!
+            if Int(splitText[index + 2]) != nil {
+                verse = Int(splitText[index + 2])!
             } else {
-                verse = returnNumber(for: splitText[4])
+                verse = returnNumber(for: splitText[index + 2])
             }
         }
         return verse

218ed12ed69495809ef0998db3f3277a59848a02
Min Kim <minmkim@gmail.com>
2018-10-04 21:55:44 -0400

    1.0

--- BibleApp/View/SettingsColorTableViewCell.swift
+++ BibleApp/View/SettingsColorTableViewCell.swift
@@ -28,7 +28,6 @@ final class SettingsColorTableViewCell: UITableViewCell {
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
     
